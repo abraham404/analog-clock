815e0c1434df4a3924bee2e8a182f1dabf387741
Jorge Rosas Windows <abraham.work7@gmail.com>
2023-12-02 19:00:17 -0700

    first commit for analog clock

---
+++ main.js
@@ -0,0 +1,21 @@
+setClock = () => {
+    //get the corrent time
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    //Calculate clock hands rotate values(according to 360deg) using the current time
+
+    hrValue = (hours * 30) + (minutes * 6) / 12;
+    minValue = minutes * 6;
+    secValue = seconds * 6;
+
+    //add calculated rotate values to the clock hand elements
+
+    document.querySelector(".hr-hand").style.transform = "rotate(" + hrValue + "deg)"
+    document.querySelector(".min-hand").style.transform = "rotate(" + minValue + "deg)"
+    document.querySelector(".sec-hand").style.transform = "rotate(" + secValue + "deg)"
+}
+
+setInterval(setClock, 1000);
